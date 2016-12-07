f85f25c9155d084debbd20dd8d06c6ae168fca39
Ted Weatherly <ted.weatherly@gmail.com>
2016-12-06 22:53:39 -0600

    Incorporate JSON fetch into snapshot21, add to examples.json

--- examples/snapshots/snapshot21/superheroes.js
+++ examples/snapshots/snapshot21/superheroes.js
@@ -1,18 +1,4 @@
 (function($) {
-	var superheroDataArray = [
-		{ "name": "Superman", "photo_url": "../../../images/avatars/superman.jpg" },
-		{ "name": "Batman", "photo_url": "../../../images/avatars/batman.jpg" },
-		{ "name": "Batgirl", "photo_url": "../../../images/avatars/batgirl.jpg" },
-		{ "name": "Daredevil", "photo_url": "../../../images/avatars/daredevil.jpg" },
-		{ "name": "Nick Cage", "photo_url": "../../../images/avatars/nickcage.jpg" },
-		{ "name": "Flash", "photo_url": "../../../images/avatars/flash.jpg" },
-		{ "name": "Cyclops", "photo_url": "../../../images/avatars/cyclops.jpg" },
-		{ "name": "Green Lantern", "photo_url": "../../../images/avatars/greenlantern.jpg" },
-		{ "name": "Colossus", "photo_url": "../../../images/avatars/colossus.jpg" },
-		{ "name": "Falcon", "photo_url": "../../../images/avatars/falcon.jpg" },
-		{ "name": "Spiderman", "photo_url": "" }
-	];
-
 	function SuperheroCardModel(name, photoUrl) {
 		var self = this;
 		self.name = name;
@@ -27,14 +13,16 @@
 		};
 	}
 
-	function SuperheroGridModel() {
+	function SuperheroGridModel(superheroDataArray) {
 		this.superheroCardModels = superheroDataArray.map(function(superheroData) {
 			return new SuperheroCardModel(superheroData.name, superheroData.photo_url);
 		});
 	}
 
 	function init() {
-		ko.applyBindings(new SuperheroGridModel(superheroDataArray));
+		$.getJSON("superheroes.json", function(superheroDataArray) {
+			ko.applyBindings(new SuperheroGridModel(superheroDataArray));
+		});
 	}
 
 	function isNonEmptyStr(str) {
